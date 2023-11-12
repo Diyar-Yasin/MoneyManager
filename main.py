f6a38981b3968912c1fe47c083ba5842a888dc67
Diyar-Yasin <diyaryasin.dy@gmail.com>
2023-11-12 16:49:19 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class MainApplication( tk.Frame ):
+    def __init__( self, parent, *args, **kwargs ):
+        tk.Frame.__init__( self, parent, *args, **kwargs )
+        self.parent = parent
+        self.parent.title( 'Money Manager' )
+        self.parent.iconbitmap( './images/appIcon.ico' )
+        # @Diyar: Add icon to app
+
+if __name__ == "__main__":
+    root = tk.Tk()    
+    MainApplication(root).pack( side="top", fill="both", expand=True )
+    root.mainloop()
